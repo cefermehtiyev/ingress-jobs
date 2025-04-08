529f1af09dc962a126c7363d7b7e61189fb452ca
Cefer Mehtiyev <cefer20051965@gmail.com>
2025-04-08 17:43:59 +0400

    initial commit

---
+++ src/test/java/com/example/ingressjobs/IngressJobsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ingressjobs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IngressJobsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
